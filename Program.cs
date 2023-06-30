8ab7c59c2a15495ca3a3e8f5cae04840260c664b
Ann <ann.mitrakhovich@gmail.com>
2023-06-30 17:52:07 +0300

    Task 37 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,55 @@
-﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+// int InputNum(string message)
+// {
+//     Console.Write(message);
+//     return int.Parse(Console.ReadLine()!);
+// }
+
+// int[] CreateArray(int size)
+// {
+//     return new int[size];
+// }
+
+// void FillArray(int[] array, int min, int max)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = rnd.Next(min, max + 1);
+// }
+// string PrintArray(int[] array)
+// {
+//     string res = String.Empty;
+//     for (int i = 0; i < array.Length; i++)
+//         res += array[i] + " ";
+//     return res;
+// }
+// int[] ChangeArray(int[] array)
+// {
+//     int length = array.Length;
+//     int[] changedArray = new int[length / 2 + length % 2];
+//     for (int i = 0; i < length / 2; i++)
+//     {
+//         changedArray[i] = array[i] * array[length - i - 1];
+//     }
+//     if (length % 2 == 1)
+//         changedArray[length / 2 - 1] = array[length / 2];
+//     return changedArray;
+// }
+// int size = InputNum("Введите размер массива: ");
+// int minValue = InputNum("Введите минимальное значение элемента: ");
+// int maxValue = InputNum("Введите максимальное значение элемента: ");
+// int[] myArray = CreateArray(size);
+// FillArray(myArray, minValue, maxValue);
+// string txt = PrintArray(myArray);
+// Console.WriteLine(txt);
+// int[] resultArray = ChangeArray(myArray);
+// string txt2 = PrintArray(resultArray);
+// Console.WriteLine(txt2);
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 // Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
@@ -28,7 +79,7 @@
 // res += array[i] + " ";
 // return res;
 // }
-// Надежда Сенчило: int CountNum(int[] array)
+// int CountNum(int[] array)
 // {
 // int index = 0;
 // int length = array.Length;
