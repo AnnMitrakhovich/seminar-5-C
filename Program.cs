082b1fff5c533bd995b36cb98dcf82b37151c9c6
Ann <ann.mitrakhovich@gmail.com>
2023-06-30 18:49:24 +0300

    Hometasks done

--- Program.cs
+++ Program.cs
@@ -45,48 +45,92 @@
 // стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+// int InputNum(string message)
+// {
+//     Console.Write(message);
+//     return int.Parse(Console.ReadLine()!);
+// }
+// int[] CreateArray(int size)
+// {
+//     return new int[size];
+// }
+// void FillArray(int[] array, int min, int max)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = rnd.Next(min, max + 1);
+// }
+// string PrintArray(int[] array)
+// {
+//     string res = String.Empty;
+//     for (int i = 0; i < array.Length; i++)
+//         res += array[i] + " ";
+//     return res;
+// }
+// int CountSumNumbers(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 1; i < array.Length; i += 2)
+//         sum += array[i];
+//     return sum;
+// }
+// int size = InputNum("Введите размер массива: ");
+// int minValue = InputNum("Введите минимальное значение элемента: ");
+// int maxValue = InputNum("Введите максимальное значение элемента: ");
+// int[] myArray = CreateArray(size);
+// FillArray(myArray, minValue, maxValue);
+// string txt = PrintArray(myArray);
+// Console.WriteLine(txt);
+// int result = CountSumNumbers(myArray);
+// Console.WriteLine($"Сумма элементов на нечетных позициях равна {result}.");
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 int InputNum(string message)
 {
     Console.Write(message);
     return int.Parse(Console.ReadLine()!);
 }
-int[] CreateArray(int size)
+double[] CreateArray(int size)
 {
-    return new int[size];
+    return new double[size];
 }
-void FillArray(int[] array, int min, int max)
+void FillArray(double[] array, int num)
 {
     Random rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-        array[i] = rnd.Next(min, max + 1);
+    for (int i = 0; i < array.Length; i++) array[i] = Math.Round(rnd.NextDouble() * num, 2);
 }
-string PrintArray(int[] array)
+string PrintArray(double[] array)
 {
     string res = String.Empty;
     for (int i = 0; i < array.Length; i++)
         res += array[i] + " ";
     return res;
 }
-int CountSumNumbers(int[] array)
+double FindMax(double[] array)
 {
-    int sum = 0;
-    for (int i = 1; i < array.Length; i += 2)
-        sum += array[i];
-    return sum;
+    double max = array[0];
+    for (int i = 1; i < array.Length; i ++)
+        if (array[i] > max) max = array[i];
+    return max;
+}
+double FindMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i ++)
+        if (array[i] < min) min = array[i];
+    return min;
 }
 int size = InputNum("Введите размер массива: ");
-int minValue = InputNum("Введите минимальное значение элемента: ");
-int maxValue = InputNum("Введите максимальное значение элемента: ");
-int[] myArray = CreateArray(size);
-FillArray(myArray, minValue, maxValue);
+int number = InputNum("Введите максимальное число для создания массива: ");
+double[] myArray = CreateArray(size);
+FillArray(myArray, number);
 string txt = PrintArray(myArray);
 Console.WriteLine(txt);
-int result = CountSumNumbers(myArray);
-Console.WriteLine($"Сумма элементов на нечетных позициях равна {result}.");
-
-
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+double minNum = FindMin(myArray);
+double maxNum = FindMax(myArray);
+double result = maxNum - minNum;
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна {result}.");
 
 
 // Задача 37: Найдите произведение пар чисел в одномерном массиве. 
