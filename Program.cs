de45ba3b380a897c012e00a441ad68408fb9c3e4
Ann <ann.mitrakhovich@gmail.com>
2023-06-30 18:32:53 +0300

    Task 36 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,95 @@
-﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+// int InputNum(string message)
+// {
+//     Console.Write(message);
+//     return int.Parse(Console.ReadLine()!);
+// }
+// int[] CreateArray(int size)
+// {
+//     return new int[size];
+// }
+// void FillArray(int[] array)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = rnd.Next(100, 1000);
+// }
+// string PrintArray(int[] array)
+// {
+//     string res = String.Empty;
+//     for (int i = 0; i < array.Length; i++)
+//         res += array[i] + " ";
+//     return res;
+// }
+// int CountNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) count++;
+//     }
+//     return count;
+// }
+// int size = InputNum("Введите размер массива: ");
+// int [] myArray = CreateArray(size);
+// FillArray(myArray);
+// string txt = PrintArray(myArray);
+// Console.WriteLine(txt);
+// Console.WriteLine("");
+// int res = CountNumbers(myArray);
+// Console.WriteLine(res);
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+void FillArray(int[] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(min, max + 1);
+}
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i] + " ";
+    return res;
+}
+int CountSumNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        sum += array[i];
+    return sum;
+}
+int size = InputNum("Введите размер массива: ");
+int minValue = InputNum("Введите минимальное значение элемента: ");
+int maxValue = InputNum("Введите максимальное значение элемента: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray, minValue, maxValue);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+int result = CountSumNumbers(myArray);
+Console.WriteLine($"Сумма элементов на нечетных позициях равна {result}.");
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
